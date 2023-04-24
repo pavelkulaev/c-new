73b28697eb0699e51d4469b97611b178939e34a7
Pavel Kulaev <pavelkulaev86@gmail.com>
2023-04-24 18:16:17 +0300

    example 5 username

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "паша")
+{
+    Console.WriteLine("Ура, это же Паша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
